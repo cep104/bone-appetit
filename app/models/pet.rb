a0d748903b0704e8b3469e81ce06583d1f64f886
cep104 <candicelizabethpeters@gmail.com>
2020-06-09 01:49:43 -0500

    updates on user show page and validations for creating a new pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,10 +4,23 @@ class Pet < ApplicationRecord
     has_one_attached :pet_img
     validates :name, presence: true
     validates_associated :pet_category,  message: "Is Already Created"
-   
+    validate :not_a_duplicate
+
+    def not_a_duplicate
+        # if there is already an ice cream with that flavor && brand, throw an error
+        pet = Pet.find_by(name: name, pet_category_id: pet_category_id, user_id: user_id)
+        if !!pet && pet != self
+          errors.add(:pet, 'has already been added.')
+        end
+      end
+
+
+
     def pet_category_attributes=(attributes)
         self.pet_category = PetCategory.find_or_create_by(attributes) if !attributes['name'].empty?
         self.pet_category
-      end
+    end
+    
+      
     
 end
