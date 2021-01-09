ac18cd65686a4f92b839b914ef46f52744b83651
cep104 <candicelizabethpeters@gmail.com>
2021-01-09 10:33:21 -0600

    commit tree dba59e36463eff8647584cbc8cc42f446cf55a4a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(name: params[:user][:name])
-        if @user && @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:user][:password])
           session[:user_id] = @user.id 
           flash[:alert] = "Incorrect Password"
           redirect_to user_path(@user)
